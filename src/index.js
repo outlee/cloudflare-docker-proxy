9a28a9bddf873549b2b88947b7d315d9a9762b4e
outlee <outlee@outlee.com>
2025-12-23 15:27:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  "docker.outlee.com": dockerHub,
+  "quay.outlee.com": "https://quay.io",
+  "gcr.outlee.com": "https://gcr.io",
+  "k8s-gcr.outlee.com": "https://k8s.gcr.io",
+  "k8s.outlee.com": "https://registry.k8s.io",
+  "ghcr.outlee.com": "https://ghcr.io",
+  "cloudsmith.outlee.com": "https://docker.cloudsmith.io",
+  "ecr.outlee.com": "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  "docker-staging.outlee.com" : dockerHub,
 };
 
 function routeByHosts(host) {
